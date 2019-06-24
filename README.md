134963aa06593f71d580eff29921cbf601e6f231
ZhengDong <zhengdong@eatigo.com>
2019-06-24 15:25:32 +0800

    font and linux build

--- README.md
+++ README.md
@@ -8,11 +8,15 @@ Uses https://github.com/SebastiaanKlippert/go-wkhtmltopdf to generate PDF files 
 # Usage
 
 - Create a Golang AWS Lambda function in the AWS console or using the AWS CLI
-- As source, use the lambda.zip file from a [release](https://github.com/SebastiaanKlippert/go-wkhtmltopdf-lambda/releases), this includes the latest version of [wkhtmltopdf](https://wkhtmltopdf.org)
-- The Handler name is `go_wkhtmltopdf_linux`
+- As source, use the lambda.zip (using 0.12.4 wkhtmltopdf general linux library)
+  (how to zip: # zip lambda.zip ./receipt_handler ./wkhtmltopdf ./wkhtmltoimage ./fonts/*)
+- The Handler name is `receipt_handler`
 - If you want to build your own version, make sure you build the go executable for Linux (GOOS=linux) and make it executable (chmod +x)
-- Create an S3 trigger for you Lambda function (using suffix `.json` is recommended)
+  (how to compile: # GOOS=linux go build receipt_handler.go)
+- Create an S3 trigger for you Lambda function (using suffix `.json`)
 - Make sure your IAM Lambda role has S3 Read and Write access to your bucket
 - Create a JSON file from the PDF generator, following the instructions at https://github.com/SebastiaanKlippert/go-wkhtmltopdf#saving-to-and-loading-from-json
 - Upload the JSON to the S3 bucket you chose in your Lambda trigger
-- The PDF is saved in the same bucket with extension `.pdf` added to the original filename
+- The PDF is saved in the same bucket with extension `.pdf` with same filename
+- The image is saved in the same bucket with extension `.jpg` with same filename
+- for the fonts, many researches need to be done for thai and traditional chinese
---
+++ receipt_handler.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/eatigo/go-wkhtmltopdf"
+)
+
+func main() {
+	lambda.Start(handler)
+}
+
+func handler(s3Event events.S3Event) error {
+	if len(s3Event.Records) > 0 {
+		os.Setenv("FONTCONFIG_PATH", "/var/task/fonts")
+		err := createPDF(s3Event.Records[0])
+		if err != nil {
+			return fmt.Errorf("create pdf error, %s", err.Error())
+		}
+		err = createImage(s3Event.Records[0])
+		if err != nil {
+			return fmt.Errorf("create image error, %s", err.Error())
+		}
+		return err
+	}
+	return nil
+}
+
+func createPDF(record events.S3EventRecord) error {
+
+	// get json file from S3
+	object, err := getS3Object(record.S3.Bucket.Name, record.S3.Object.Key)
+	if err != nil {
+		return err
+	}
+	defer object.Close()
+
+	// make sure we look for the included wkhtmltopdf binary
+	os.Setenv("WKHTMLTOPDF_PATH", os.Getenv("LAMBDA_TASK_ROOT"))
+
+	// create PDF generator
+	pdfg, err := wkhtmltopdf.NewPDFGeneratorFromJSON(object)
+	if err != nil {
+		return err
+	}
+
+	// create PDF
+	err = pdfg.Create()
+	if err != nil {
+		return err
+	}
+
+	// write PDF to same filename with .pdf added
+	return putS3Object(record.S3.Bucket.Name, strings.Replace(record.S3.Object.Key, ".json", ".pdf", -1), pdfg.Bytes())
+}
+
+func createImage(record events.S3EventRecord) error {
+
+	// get json file from S3
+	object, err := getS3Object(record.S3.Bucket.Name, record.S3.Object.Key)
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+	defer object.Close()
+
+	// make sure we look for the included wkhtmltoimage binary
+	os.Setenv("WKHTMLTOIMAGE_PATH", os.Getenv("LAMBDA_TASK_ROOT"))
+
+	// create image
+	image, err := wkhtmltopdf.ImageFromJSON(object)
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	// write PDF to same filename with .png added
+	return putS3Object(record.S3.Bucket.Name, strings.Replace(record.S3.Object.Key, ".json", ".jpg", -1), image)
+}
+
+func getS3Object(bucket, key string) (io.ReadCloser, error) {
+
+	sess, err := session.NewSession()
+	if err != nil {
+		return nil, fmt.Errorf("error creating AWS session (check access keys): %s", err)
+	}
+
+	in := &s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	}
+
+	obj, err := s3.New(sess).GetObject(in)
+	if err != nil {
+		return nil, fmt.Errorf("error getting S3 object: %s", err)
+	}
+	return obj.Body, nil
+}
+
+func putS3Object(bucket, key string, buf []byte) error {
+
+	sess, err := session.NewSession()
+	if err != nil {
+		return fmt.Errorf("error creating AWS session (check access keys): %s", err)
+	}
+
+	in := &s3.PutObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+		Body:   bytes.NewReader(buf),
+	}
+
+	_, err = s3.New(sess).PutObject(in)
+	if err != nil {
+		return fmt.Errorf("error putting S3 object: %s", err)
+	}
+	return nil
+}
